8b5c18d06b277270898f2cb8868aed3168f2bb46
Hariharan <hariharan@aigamer.dev>
2025-11-29 18:47:48 +0530

    Update V1 portfolio: styles, scripts, images and add logos

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,42 @@
-// Minimal scripts - jQuery and other libraries loaded from CDN
-(function ($) { "use strict"; $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () { if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) { var t = $(this.hash); if ((t = t.length ? t : $("[name=" + this.hash.slice(1) + "]")).length) return $("html, body").animate({ scrollTop: t.offset().top }, 1e3, "easeInOutExpo"), !1 } }); $(".js-scroll-trigger").click(function () { $(".navbar-collapse").collapse("hide") }); $("body").scrollspy({ target: "#sideNav" }) })(jQuery);
+/*!
+    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+})(jQuery); // End of use strict
